ac3c0ae6e8cd7d9b358894eb2d0044670a63cd3a
Jalique Gordon <jaliquegordon@gmail.com>
2024-02-12 22:08:18 -0400

    Made Final Changes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
-
+use App\Http\Controllers\AboutController;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/about', [AboutController::class, 'show']); {
+    return "My About Page"; 
+};
